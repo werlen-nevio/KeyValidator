c71e00a32ee0a386b486c08fd814bb2332beab99
Nevio Werlen <LordNevi@outlook.com>
2024-09-10 11:39:58 +0200

    add config and class

---
+++ src/main.js
@@ -0,0 +1,27 @@
+class KeyValidator {
+    constructor() {
+        this.config = null;
+    }
+  
+    async init() {
+        const config = await this.loadConfig();
+        this.config = config;
+        // todo: make all forms with class not submittable
+    }
+  
+    async loadConfig() {
+        try {
+            const response = await fetch('config.json', {cache: "reload"});
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const config = await response.json();
+            return config;
+
+        }catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
+    }
+}
